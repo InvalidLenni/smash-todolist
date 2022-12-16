0344136b373d04e044d28108fc506db284935af3
InvalidLenni <contact@invalidlenni.de>
2022-12-16 17:52:22 +0100

    add useEffect Code * This new code part is adding useEffect which is literally re-rendering the components after the taskStateArray Update and localStorage "tasks" update.

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import EventHandler from "./EventHandler";
 
 export default function Display() {
-  // Display main functions
-  //
-  return (
-    <div className="Display">
-      <section id="display">
-        <div className="shadow-xl base-300 flex flex-col w-full lg:flex-row">
-          <EventHandler />
-        </div>
-      </section>
-    </div>
-  );
+	// Display main functions
+	//
+	return (
+		<div className="Display">
+			<section id="display">
+				<EventHandler />
+			</section>
+		</div>
+	);
 }
